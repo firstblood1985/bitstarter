2b182ffa8c7bb2b6764127622dd6f982b56a1717
firstblood1985 <limin.hust@foxmail.com>
2013-07-07 16:44:55 +0800

    hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs=require("fs");
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  var buffer = new Buffer(1024);
+  buffer = fs.readFileSync("./index.html");
+  //console.log(buffer);
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
